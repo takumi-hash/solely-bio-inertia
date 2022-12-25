e982601a709fa42a0759edd47c65ae412b95b051
takumi-hash <39505885+takumi-hash@users.noreply.github.com>
2022-12-25 14:42:02 +0900

    style minor fix

--- resources/js/Pages/Card/Show.jsx
+++ resources/js/Pages/Card/Show.jsx
@@ -1,23 +1,28 @@
+import GuestLayout from '@/Layouts/GuestLayout';
 import { Head } from '@inertiajs/inertia-react';
 
 export default function ShowCard({ user }) {
     return (
         <>
             <Head title="Show Profile" />
-            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-                <div className="fixed top-0 right-0 px-6 py-4 sm:block">
-                    {user ? (<>
-                        <p>User Card</p>
-                        <p>{user.name}</p>
-                        <p>{user.handlename}</p>
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            {user ? (<>
+                                <p>User Card</p>
+                                <p>{user.name}</p>
+                                <p>{user.handlename}</p>
 
-                    </>
+                            </>
 
-                    ) : (
-                        <>
-                            <p>User is undefined.</p>
-                        </>
-                    )}
+                            ) : (
+                                <>
+                                    <p>User is undefined.</p>
+                                </>
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
