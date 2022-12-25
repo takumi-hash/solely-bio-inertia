bde26ad96667a2a9c5569ce35dbab6a2677f930c
takumi.hashimoto <takumi.hashimoto@rakuten.com>
2022-12-25 20:24:57 +0900

    style minor update

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -14,7 +14,7 @@ export default function Authenticated({ auth, header, children }) {
             <nav className="bg-white border-b border-gray-100">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
-                        <div className="flex">
+                        <div className="flex items-center">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
                                     <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
